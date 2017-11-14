7f9bfa24caede3ab2befe36bcb03461fd7b4a837
Andres Muñoz <andres.munoz.b@usach.cl>
2017-11-13 21:55:09 -0300

    matriz

--- estructuras.h
+++ estructuras.h
@@ -1,6 +1,12 @@
 #ifndef _STRUCTS_H_
 #define _STRUCTS_H_
 
+typedef struct matriz{
+    int **data;
+    int row;
+    int col;
+}Matriz;
+
 
 
 #endif
--- main.c
+++ main.c
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 #include "funciones.h"
 
+int main(int argc,char** argv){
+
+	Matriz* matriz = matriz_create(10,10);
+	matriz_show(matriz);
+	matriz_destroy(matriz);
+	return 1;
+}
